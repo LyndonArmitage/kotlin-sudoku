b6f74aae0010ad7b8e62e1dec88271310a3a986e
Lyndon Armitage <lyndon.armitage@gmail.com>
2020-04-07 00:05:10 +0100

    Add basic HashMapSudokuGrid

---
+++ src/main/kotlin/codes/lyndon/sudoku/mutable/HashMapSudokuGrid.kt
@@ -0,0 +1,75 @@
+package codes.lyndon.sudoku.mutable
+
+import codes.lyndon.sudoku.SudokuCell
+import codes.lyndon.sudoku.SudokuGrid
+import codes.lyndon.sudoku.immutable.ImmutableCellGroup
+import codes.lyndon.sudoku.renderer.BasicTextRenderer
+
+class HashMapSudokuGrid : MutableSudokuGrid<ImmutableCellGroup> {
+
+    private val map: HashMap<Pos, Int?> = HashMap(
+        SudokuGrid.cellsPerColumn * SudokuGrid.cellsPerRow
+    )
+
+    private data class Pos(val x: Int, val y: Int)
+
+    override fun setCellAt(x: Int, y: Int, value: Int?) {
+        val pos = posFor(x, y)
+        map[pos] = value
+    }
+
+    override fun cellAt(x: Int, y: Int): Int? {
+        val pos = posFor(x, y)
+        return map.getOrDefault(pos, null)
+    }
+
+    override fun boxAt(boxX: Int, boxY: Int): ImmutableCellGroup {
+        val cells = HashSet<SudokuCell>(9)
+        val startX: Int = boxX * 3
+        val startY: Int = boxY * 3
+        for (x in startX until startX + 3) {
+            for (y in startY until startY + 3) {
+                cells.add(SudokuCell(x, y, get(x, y)))
+            }
+        }
+        return ImmutableCellGroup(cells)
+    }
+
+    override fun rowAt(y: Int): ImmutableCellGroup {
+        val cells = HashSet<SudokuCell>(SudokuGrid.cellsPerRow)
+        for (x in 0 until SudokuGrid.cellsPerRow) {
+            cells.add(SudokuCell(x, y, get(x, y)))
+        }
+        return ImmutableCellGroup(cells)
+    }
+
+    override fun coulmnAt(x: Int): ImmutableCellGroup {
+        val cells = HashSet<SudokuCell>(SudokuGrid.cellsPerColumn)
+        for (y in 0 until SudokuGrid.cellsPerColumn) {
+            cells.add(SudokuCell(x, y, get(x, y)))
+        }
+        return ImmutableCellGroup(cells)
+    }
+
+    override fun toString(): String = BasicTextRenderer.render(this)
+
+    private fun posFor(x: Int, y: Int): Pos =
+        Pos(x, y) // TODO: Pool x,y coordinates
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as HashMapSudokuGrid
+
+        if (map != other.map) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return map.hashCode()
+    }
+
+
+}
--- src/main/kotlin/codes/lyndon/sudoku/mutable/MutableSudokuGrid.kt
+++ src/main/kotlin/codes/lyndon/sudoku/mutable/MutableSudokuGrid.kt
@@ -1,8 +1,9 @@
 package codes.lyndon.sudoku.mutable
 
+import codes.lyndon.sudoku.CellGroup
 import codes.lyndon.sudoku.SudokuGrid
 
-interface MutableSudokuGrid : SudokuGrid<MutableCellGroup> {
+interface MutableSudokuGrid<CellGroupType : CellGroup> : SudokuGrid<CellGroupType> {
 
     fun setCellAt(x: Int, y: Int, value: Int?)
 
