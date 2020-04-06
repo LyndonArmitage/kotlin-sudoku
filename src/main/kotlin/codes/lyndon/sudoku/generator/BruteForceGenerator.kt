46dab45950734831f4916b1da2cd182bd869af50
Lyndon Armitage <lyndon.armitage@gmail.com>
2020-04-07 00:30:30 +0100

    Refactor BruteForceGenerator to use mutable

--- src/main/kotlin/codes/lyndon/sudoku/generator/BruteForceGenerator.kt
+++ src/main/kotlin/codes/lyndon/sudoku/generator/BruteForceGenerator.kt
@@ -2,6 +2,7 @@ package codes.lyndon.sudoku.generator
 
 import codes.lyndon.sudoku.*
 import codes.lyndon.sudoku.immutable.ImmutableSudokuGrid
+import codes.lyndon.sudoku.mutable.HashMapSudokuGrid
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.random.Random
@@ -13,29 +14,32 @@ class BruteForceGenerator(
     private data class Pos(val x: Int, val y: Int)
 
     override fun generate(): SudokuGrid<CellGroup> {
-        lateinit var sudoku: SudokuGrid<CellGroup>
+        val sudoku = HashMapSudokuGrid()
+        var count: Long = 0L
         do {
-            sudoku = ImmutableSudokuGrid.build {
-                val positions = ArrayList<Pos>(81)
-                for(x in 0 until 9) {
-                    for (y in 0 until 9) {
-                        positions.add(Pos(x, y))
-                    }
-                }
-                positions.shuffle(random)
-                val queue = ArrayDeque(positions)
-                while (queue.isNotEmpty()) {
-                    val pos = queue.last
-                    var isValid: Boolean
-                    do {
-                        val value = random.nextInt(9) + 1
-                        set(pos.x, pos.y, value)
-                        isValid = this.build().isValid(mustBeComplete = false)
-                    } while (!isValid)
-                    queue.removeLast()
+            val positions = ArrayList<Pos>(81)
+            for (x in 0 until 9) {
+                for (y in 0 until 9) {
+                    positions.add(Pos(x, y))
                 }
             }
+            positions.shuffle(random)
+            val queue = ArrayDeque(positions)
+            while (queue.isNotEmpty()) {
+                val pos = queue.last
+                var isValid: Boolean
+                do {
+                    val value = random.nextInt(9) + 1
+                    sudoku[pos.x, pos.y] = value
+                    isValid = sudoku.isValid(mustBeComplete = false)
+                } while (!isValid)
+                queue.removeLast()
+            }
 
+            count ++
+            if (count % 1000L == 0L) {
+                println(count)
+            }
         } while (!sudoku.isValid(mustBeComplete = true))
         return sudoku
     }
