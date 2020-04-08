c56cb6c1b034b364048e60402820b2cbbd3d15d7
Lyndon Armitage <lyndon.armitage@gmail.com>
2020-04-08 14:31:59 +0100

    Add common object pooling to Pos
    Now we will always reuse the same instances of Pos when we are requesting from
    the common 9x9 Sudoku grid.
    
    In theory this should cut down on memory usage

--- src/main/kotlin/codes/lyndon/sudoku/mutable/Pos.kt
+++ src/main/kotlin/codes/lyndon/sudoku/mutable/Pos.kt
@@ -1,10 +1,29 @@
 package codes.lyndon.sudoku.mutable
 
+import codes.lyndon.sudoku.Sudoku
+import java.util.concurrent.ConcurrentHashMap
+
 class Pos private constructor(val x: Int, val y: Int) {
 
     companion object {
+
+        private val commonObjectPool = ConcurrentHashMap<Pair<Int, Int>, Pos>()
+
+        init {
+            // create a fixed set of shared objects for all common x,y coordinates
+            for (x in 0 until Sudoku.cellsPerRow) {
+                for (y in 0 until Sudoku.cellsPerColumn) {
+                    val pos = Pos(x, y)
+                    commonObjectPool[Pair(x, y)] = pos
+                }
+            }
+        }
+
         fun of(x: Int, y: Int): Pos =
-            Pos(x, y) // TODO: Pool x,y coordinates
+            commonObjectPool[Pair(x, y)] ?: Pos(x, y)
+        // We end up creating a Pair(x, y) every invocation but it will be
+        // caught very early by GC compared to having multiple Pos(x, y) for
+        // the same given (x,y) coordinates.
     }
 
     override fun equals(other: Any?): Boolean {
